31fa183b1e2e09f88817302d7731fb86cc9761ae
I1-P221 <I1-P221@Pinfo1.local>
2025-08-19 17:21:36 +0100

    First Commit

---
+++ app/src/main/java/com/example/gestionnairedetches/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.gestionnairedetches
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.gestionnairedetches.ui.theme.GestionnaireDeTâchesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GestionnaireDeTâchesTheme {
+            Surface (modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background)
+            {
+                TaskCompleted()
+            }
+            }
+        }
+    }
+}
+
+@Composable
+fun TaskCompleted(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.ic_task_completed)
+
+    Column(
+        modifier = modifier
+            .fillMaxSize(), // Prend tout l'espace disponible
+        verticalArrangement = Arrangement.Center, // Centre verticalement
+        horizontalAlignment = Alignment.CenterHorizontally // Centre horizontalement
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Text(
+            text = "All tasks completed",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
+        )
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    GestionnaireDeTâchesTheme {
+        TaskCompleted()
+    }
+}
